3af8498078ad00364a700351fff05a01ed832700
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-01-19 13:03:57 -0300

    feat: :sparkles: add header component with basic structure
    - Implemented Header component with its template.
    - Implemented Footer component with its template.
    - Added unit tests for both Header and Footer components.
    - Updated app component to include Header.
    - Removed unused styles and added Tailwind CSS for styling.
    - Updated index.html to reflect changes in component structure.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layout/components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [],
+  templateUrl: './app.html'
 })
 export class App {
   protected readonly title = signal('EventDex');
